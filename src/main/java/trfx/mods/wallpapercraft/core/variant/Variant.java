6b4f27c5d2146e3ac4861f2fee6ec7bba8126b80
Alexander Laptev <alexander_laptev4@inbox.ru>
2024-09-03 15:15:55 +0300

    implement Variant

--- src/main/java/trfx/mods/wallpapercraft/core/variant/Variant.java
+++ src/main/java/trfx/mods/wallpapercraft/core/variant/Variant.java
@@ -1,13 +1,47 @@
 package trfx.mods.wallpapercraft.core.variant;
 
-import java.util.Set;
+import org.apache.commons.lang3.Validate;
+import trfx.mods.wallpapercraft.core.csv.CsvTable;
+
+import java.util.*;
 
 public class Variant {
+    private final Set<String> internalNames;
+
+    private final Map<String, Map<String, String>> localizedLookup;
+
+    public Variant(CsvTable table) {
+        // Validate
+        for (int row = 1; row < table.getRowCount(0); row++) {
+            Validate.notEmpty(table.get(row, 0), "Variant with no internal name found in row %d", row);
+        }
+
+        // Fill internal names
+        Set<String> names = new HashSet<>();
+        for (int row = 1; row < table.getRowCount(0); row++) {
+            String name = table.get(row, 0);
+            Validate.isTrue(names.add(name), "Duplicate of variant '%s' found in row %d", name, row);
+        }
+        this.internalNames = Collections.unmodifiableSet(names);
+
+        // Fill lookup
+        Map<String, Map<String, String>> lookup = new HashMap<>();
+        for (int row = 1; row < table.getRowCount(0); row++) {
+            String internal = table.get(row, 0);
+            Map<String, String> locales = new HashMap<>();
+            for (int column = 1; column < table.getColumnCount(); column++) {
+                locales.put(table.get(0, column), table.get(row, column));
+            }
+            lookup.put(internal, Collections.unmodifiableMap(locales));
+        }
+        this.localizedLookup = Collections.unmodifiableMap(lookup);
+    }
+
     public Set<String> getInternalNames() {
-        throw new UnsupportedOperationException(); // TODO
+        return internalNames;
     }
 
     public String getLocalizedName(String internalName, String locale) {
-        throw new UnsupportedOperationException(); // TODO
+        return localizedLookup.get(internalName).get(locale);
     }
 }
